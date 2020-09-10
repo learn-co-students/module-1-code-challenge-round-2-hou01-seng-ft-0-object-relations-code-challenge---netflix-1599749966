17c72b342a800649d4f8ee82f7c935f020054b3a
Hugo Rodriguez <hugor37@gmail.com>
2020-09-10 11:30:54 -0500

    submission for deadline

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
 class Review
 
+    attr_reader :viewer, :movie, :rating
+
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,4 +1,5 @@
 class Viewer
+
   attr_accessor :username
 
   @@all = []
@@ -11,5 +12,25 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def reviews
+    Review.all.select {|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.collect {|review| review.movie}
+  end
+
+  def reviewed_movie?(movie)
+    reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating) # For this method, I wanted to use reviewed_movies method again
+    if reviewed_movie?(movie) # if this is true, then we replace the old rating with the new one (idk if it was used correctly)
+      Review.rating = rating # this line updates the rating
+    else     # if false, we make a new review
+      Review.new(self, movie, rating) # this line makes the new review
+    end
+  end # I couldn't figure it out. I get a no method error for 'rating='. I even changed the attr_reader to accessor but it didn't work.
+
 end
