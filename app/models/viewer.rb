fcffac3849acf2c6d1e7746c2aa9487f78e7a140
MSarfarazH <m.s.hussain@outlook.com>
2020-09-10 11:44:05 -0500

    submission for deadline

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -12,4 +12,20 @@ class Viewer
     @@all
   end
   
+  def reviews
+    Review.all.select{|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    self.reviews.collect{|viewer| viewer.movie}
+  end
+
+  def reviewed_movie?(movie)
+    self.reviewed_movies.any?(movie)
+  end 
+
+   def rate_movie(movie, rating)
+     Review.movie.any?(movie, rating)
+   end
+  
 end
